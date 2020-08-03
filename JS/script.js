ee9cbfd2b57ba2e12ff9e547fea71d4d7cc88ad4
Maxim <novgorodzev@mail.ru>
2020-08-03 19:32:37 +1100

    0308201932

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,20 @@
 
 "use strict";
 
+function first() {
+setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+
+function learnJS(lang, callback) {
+    
+}
