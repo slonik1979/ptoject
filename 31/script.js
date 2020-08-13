f70a13d768871be6749207e26a50441461208477
Maxim <603710@gmail.com>
2020-08-13 17:46:37 +1100

    1308201746

--- 31/script.js
+++ 31/script.js
@@ -14,5 +14,4 @@ const deleteElement = (event) => {
 
 }; 
 
-
 btr.addEventListener('click', deleteElement);
---
+++ 32/script.js
@@ -0,0 +1,9 @@
+
+
+
+//console.log(document.body.childNodes);
+
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+console.log(document.querySelector('#current').parentNode.parentNode);
