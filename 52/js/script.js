e1d4f25c60bc60dd78f681a4b3af20dd6044f3ff
Maxim <novgorodzev@mail.ru>
2020-09-17 22:39:43 +1100

    1709202239

---
+++ 52/js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const inputRub = document.querySelector('#rub'),
+    inputUsd = document.querySelector('#usd');
+
+    inputRub.addEventListener('input', () => {
+        const request = new XMLHttpRequest();
+
+        request.open();
+
+    })
