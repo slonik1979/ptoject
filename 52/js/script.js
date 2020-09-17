e774e8ee007491e4799463f38e4e38ed11a03255
Maxim <novgorodzev@mail.ru>
2020-09-17 23:06:11 +1100

    1709202306

--- 52/js/script.js
+++ 52/js/script.js
@@ -6,6 +6,16 @@ const inputRub = document.querySelector('#rub'),
     inputRub.addEventListener('input', () => {
         const request = new XMLHttpRequest();
 
-        request.open();
+        request.open('GET', 'js/current1.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
 
-    })
+        request.addEventListener('load', () => {
+            if (request.status === 200) {
+                const data = JSON.parse(request.response);
+                inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+            } else {
+                inputUsd.value = "Что-то пошло не так";
+            }
+        });
+    });
