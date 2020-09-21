cd68d6c45fee59198686761ef524a8983d90e997
Maxim <603710@gmail.com>
2020-09-21 12:05:32 +1100

    2109201205

--- 43-44-47-53/js/script.js
+++ 43-44-47-53/js/script.js
@@ -209,6 +209,58 @@ window.addEventListener('DOMContentLoaded', () => {
             21,
             ".menu .container"
         ).render();
-    
+
+        //Формы
+
+        const forms = document.querySelectorAll('form');
+
+        const message = {
+            loading: 'Загрузка',
+            succes: 'Спасибо! Скоро мы с Вами свяжемся',
+            failure: 'Что-то пошло не так'
+        };
+
+        forms.forEach(item => {
+            postData(item);
+        });
+
+        function postData (form) {
+            form.addEventListener('submit', (event) => {
+                event.preventDefault();
+
+                const statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                statusMessage.textContent = message.loading;
+                form.append(statusMessage);
+
+                const request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+
+                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+                const formData = new FormData(form);
+
+                const object = {};
+                    formData.forEach(function(value, key){
+                        object[key] = value;
+                    });
+               
+                    const json = JSON.stringify(object);
+
+                request.send(json);
+                request.addEventListener('load', () => {
+                    if (request.status === 200) {
+                        console.log(request.response);
+                        statusMessage.textContent = message.succes;
+                        form.reset();
+                        setTimeout( () => {
+                            statusMessage.remove();
+                        }, 2000);
+                    } else {
+                        statusMessage.textContent = message.failure;
+                    }
+                });
+            });
+        }
+   
     });
     
