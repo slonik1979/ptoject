0f9c2fabf66149282ba899ae20efca2f2eb34462
Maxim <603710@gmail.com>
2021-01-21 17:32:26 +1100

    2101211730

--- PULS/gulpfile.js
+++ PULS/gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(cleanCSS({compatibility: 'ie8'}))
