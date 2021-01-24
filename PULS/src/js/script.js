62877d2c15c5659ba3660b5318e1e104ed6316cc
Maxim <novgorodzev@mail.ru>
2021-01-24 18:44:00 +1100

    2401211843

--- PULS/src/js/script.js
+++ PULS/src/js/script.js
@@ -1,17 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
+    $(document).ready(function(){
+        $('.carousel__inner').slick(
+            {                
+                speed: 1200,
+                adaptiveHeight: true,
+                fade: true,
+                cssEase: 'linear'
+             }
+        );
+      });
 })
