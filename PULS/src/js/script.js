6c34e3014d18b5146b7811009ecb2ab1fac3b3f8
Maxim <603710@gmail.com>
2021-01-25 17:57:32 +1100

    2501211754

--- PULS/src/js/script.js
+++ PULS/src/js/script.js
@@ -3,9 +3,11 @@ window.addEventListener('DOMContentLoaded', () => {
         $('.carousel__inner').slick(
             {                
                 speed: 1200,
-                adaptiveHeight: true,
+                // adaptiveHeight: true,
                 fade: true,
-                cssEase: 'linear'
+                cssEase: 'linear',
+                prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/left.svg"></button>',
+                nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/right.svg"</button>'
              }
         );
       });
