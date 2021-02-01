083cf121ea9e51dbf1ac90aee446d2daa4c3eb61
Maxim <603710@gmail.com>
2021-02-01 15:31:40 +1100

    0102211531

--- PULS/src/js/script.js
+++ PULS/src/js/script.js
@@ -96,6 +96,22 @@ window.addEventListener('DOMContentLoaded', () => {
         return false;
       });
 
+      //pageup
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+        $("a[href^='#']").click(function(){
+          const _href = $(this).attr("href");
+          $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+          return false;
+  });
+      });
+
+      new WOW().init();
+
 
       });
 });
