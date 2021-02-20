2563eee883f35863273b96287142a14e2fda7f2f
Maxim <novgorodzev@mail.ru>
2021-02-20 22:16:01 +1100

    2002212215

--- JAVA SCRIPT 14022021/js/script.js
+++ JAVA SCRIPT 14022021/js/script.js
@@ -82,6 +82,38 @@
 // console.log(parseFloat(test));
 
 
+// Callback- функции
+
+function first() {
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done () {
+    console.log("Я прошел этот урок");
+}
+
+learnJS('JavaScript', done);
+
+
+learnJS('JavaScript', function() {
+    console.log("Я прошел этот урок");
+});
+ 
+
 
 
 
