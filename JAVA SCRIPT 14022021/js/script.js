0536f0ba724e84204ccf35d8636f8e5cb3980cec
Maxim <novgorodzev@mail.ru>
2021-02-24 22:21:50 +1100

    2402212221

--- JAVA SCRIPT 14022021/js/script.js
+++ JAVA SCRIPT 14022021/js/script.js
@@ -84,37 +84,54 @@
 
 // Callback- функции
 
-function first() {
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+// function first() {
+//     setTimeout(function() {
+//         console.log(1);
+//     }, 500);
+// }
 
-function second() {
-    console.log(2);
-}
+// function second() {
+//     console.log(2);
+// }
 
-first();
-second();
+// first();
+// second();
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
 
-function done () {
-    console.log("Я прошел этот урок");
-}
+// function done () {
+//     console.log("Я прошел этот урок");
+// }
 
-learnJS('JavaScript', done);
+// learnJS('JavaScript', done);
 
 
-learnJS('JavaScript', function() {
-    console.log("Я прошел этот урок");
-});
+// learnJS('JavaScript', function() {
+//     console.log("Я прошел этот урок");
+// });
  
 
 
+// Объекты, деструктуризация объектов (ES6)
+
+const options = {
+    name: 'test',
+    widht: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+console.log(options.name);
+
+delete options.name;
+
+console.log(options);
 
 
 
