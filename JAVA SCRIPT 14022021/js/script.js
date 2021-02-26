b58655708d7b6e2c81be14539100d21dc76ce298
Maxim <603710@gmail.com>
2021-02-26 17:06:56 +1100

    2602211706

--- JAVA SCRIPT 14022021/js/script.js
+++ JAVA SCRIPT 14022021/js/script.js
@@ -159,6 +159,7 @@
 // console.log(counter);
 
 
+
 // Массивы и псевдомассивы
 
 // const arr = [1, 2, 3, 6, 8, 12];
@@ -173,7 +174,6 @@
 // function compareNum(a, b) {
 //     return a -b;
 // }
-
 
 // arr.forEach(function(item, i, arr) {
 //     console.log(`${i}: ${item} внутри ${arr}`);
@@ -202,33 +202,54 @@
 // Передача по ссылке или по значению, Spread
 // оператор (ES6-ES9)
 
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejopurnal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];  
+// const video = ['youtube', 'vimeo', 'rutube'],
+//       blogs = ['wordpress', 'livejopurnal', 'blogger'],
+//       internet = [...video, ...blogs, 'vk', 'facebook'];  
 
-console.log(internet);
+// console.log(internet);
 
-const object = {
-    name: 'test',
-    widht: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-            }
-};
+// const object = {
+//     name: 'test',
+//     widht: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//             }
+// };
 
-const newObject = {...object};
-    newObject.name = 'hello';
+// const newObject = {...object};
+//     newObject.name = 'hello';
+//     newObject.colors.border = 'green';
 
-console.log(object);
-console.log(newObject);
+// console.log(object);
+// console.log(newObject);
+
+
 
 
 
 
 // Основы ООП, прототипно-ориентированное
 // наследование
+
+// const soldier = {
+//     health: 100,
+//     armor: 400,
+//     sayHello: function() {
+//         console.log("Hello");
+//     }
+// };
+
+// const ivan =Object.create(soldier);
+// ivan.sayHello();
+// Object.setPrototypeOf(ivan, soldier);
+
+// console.log(ivan.armor);
+
+
+
+
 
 
 
@@ -281,71 +302,81 @@ genres
 
 P.S. Функции вызывать не обязательно */
 
-// let numberOfFilms;
 
-// function start() {
-//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//     }
-// }
-
-// start();
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
+/* 4 Задание на урок:
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
 
 
-// function rememberMyFilms() {
-//     for (let i = 0; i < 2; i++) {
-//         const a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
-//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//     } else {
-//         i--;
-//     }
-//     }
-// }
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms:function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+        }
+    },
+    personalLevel:function () {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+            }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+                console.log("Вы классический зритель");
+            } else if (personalMovieDB.count >= 30) {
+                console.log("Вы киноман");
+            } else {
+                console.log("Произошла ошибка");
+            }
+    },
+    showMyDb: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i < 4; i++ ) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (genre === '' || genre == null) {
+                console.log("Вы ввели неправильно!");
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] =genre;
+            }
+        } 
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1}- это ${item}`);
+        });
+    }
+};
+personalMovieDB.writeYourGenres();
 
-// // rememberMyFilms();
-
-
-
-
-// function personalLevel() {
-//         if (personalMovieDB.count < 10) {
-//             console.log("Просмотрено довольно мало фильмов");
-//             }else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//                 console.log("Вы классический зритель");
-//             } else if (personalMovieDB.count >= 30) {
-//                 console.log("Вы киноман");
-//             } else {
-//                 console.log("Произошла ошибка");
-//             }
-//     }
-
-// // personalLevel();
-
-// function showMyDb(hidden) {
-//     if (!hidden) {
-//         console.log(personalMovieDB);
-//     }
-// }
-
-// showMyDb(personalMovieDB.privat);
-
-// function writeYourGenres() {
-//     for (let i = 1; i < 4; i++ ) {
-//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);;
-//     } 
-// }
-
-// writeYourGenres();
