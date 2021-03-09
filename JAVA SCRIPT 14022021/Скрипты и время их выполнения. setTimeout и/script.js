d54f65f252312c77b1686d9bee2578639eda89ff
Maxim <novgorodzev@mail.ru>
2021-03-10 00:07:06 +1100

    1003210006

--- JAVA SCRIPT 14022021/Скрипты и время их выполнения. setTimeout и/script.js
+++ JAVA SCRIPT 14022021/Скрипты и время их выполнения. setTimeout и/script.js
@@ -2,17 +2,40 @@ const btn = document.querySelector('.btn');
 let timerId,
   i = 0;
 
-btn.addEventListener('click', () => {
-    //const timeId = setTimeout(logger, 2000);
-    timerId = setInterval(logger, 500);
-});
+function myAnimation(){
+    const elem = document.querySelector('.box');
+    let pos = 0;
 
-
-
-function logger(){
-    if ( i === 3){
-        clearInterval(timerId);
+    const id = setInterval(fram, 10);
+    function fram(){
+        if (pos == 300) {
+            clearInterval();
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
     }
-    console.log('text');
-    i++;
 }
+
+btn.addEventListener('click', myAnimation);
+
+
+// btn.addEventListener('click', () => {
+//     //const timeId = setTimeout(logger, 2000);
+//     timerId = setInterval(logger, 500);
+// });
+
+// function logger(){
+//     if ( i === 3){
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+
+// let id = setTimeout(function log(){
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// }, 500);
