8bc36ede4b6f7296db918ca98e5b5ea9acdae2f4
Maxim <novgorodzev@mail.ru>
2021-04-01 23:31:36 +1100

    0104212330

--- PULS_AUTUPORT/src/mailer/smart.php
+++ PULS_AUTUPORT/src/mailer/smart.php
@@ -11,15 +11,15 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'novgorodzev@mail.ru';                 // Наш логин
+$mail->Password = 'zhelnin972021zhelnin';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('novgorodzev@mail.ru', 'Pulse');   // От кого письмо 
+$mail->addAddress('avtoportobmen@list.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
