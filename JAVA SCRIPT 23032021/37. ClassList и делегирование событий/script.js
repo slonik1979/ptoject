18123313f011a6b254832cb43c4ce877a4081f85
Maxim <novgorodzev@mail.ru>
2021-04-13 22:22:31 +1100

    13/04/21 2222

--- JAVA SCRIPT 23032021/37. ClassList и делегирование событий/script.js
+++ JAVA SCRIPT 23032021/37. ClassList и делегирование событий/script.js
@@ -5,3 +5,11 @@ const btns = document.querySelectorAll('button');
 btns[0].classList.add('red');   /// добавляет класс
 btns[0].classList.remove('blue');  // удаляет класс
 btns[0].classList.toggle('blue');
+
+btns[0].addEventListener( 'click', () => {
+if (!btns[1].classList.contains('red')) {
+    btns[1].classList.add('red');
+} else {
+    btns[1].classList.remove('red');
+}
+});
