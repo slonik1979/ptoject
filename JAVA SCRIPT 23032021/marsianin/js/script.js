d528d888a79fe2cd13b7157a4c16639eb46bae5a
Maxim <603710@gmail.com>
2021-04-30 17:35:56 +1100

    3004211735

--- JAVA SCRIPT 23032021/marsianin/js/script.js
+++ JAVA SCRIPT 23032021/marsianin/js/script.js
@@ -1,3 +1,7 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    
 /* Задания на урок:
 
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
@@ -12,7 +16,7 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+
 
 const movieDB = {
     movies: [
@@ -27,7 +31,22 @@ const movieDB = {
 const advertising = document.querySelectorAll('.promo__adv img'),
     genre = document.querySelector('.promo__genre'),
     promo = document.querySelector('.promo__bg'),
-    moviesList = document.querySelector('.promo__interactive-list');
+    moviesList = document.querySelector('.promo__interactive-list'),
+    addForm = document.querySelector('form.add'),
+    addInput = addForm.querySelector('.adding__input'),
+    checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const newFilm = addForm.value;
+        const favorite = checkbox.checked;
+
+        movieDB.movies.push(newFilm);
+        movieDB.movies.sort();
+    });
+
+    console.log(movieDB.movies);
 
 advertising.forEach(item => {
     item.remove();
@@ -37,14 +56,30 @@ genre.textContent = 'драма';
 
 promo.style.cssText = 'background: url("../img/bg.jpg")';
 
-moviesList.innerHTML = "";
-
 movieDB.movies.sort();
 
-movieDB.movies.forEach((item, i) => {
+function creatMovieList() {
+    moviesList.innerHTML = "";
+    movieDB.movies.forEach((item, i) => {
     moviesList.innerHTML = moviesList.innerHTML + `
     <li class="promo__interactive-item">${i + 1}. ${item}
         <div class="delete"></div>
     </li>
     `;
+});
+}
+
+
+/* Задания на урок:
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+5) Фильмы должны быть отсортированы по алфавиту */
+
 });
