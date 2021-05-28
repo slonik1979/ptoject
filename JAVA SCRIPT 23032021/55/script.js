fa7190ceceb44e7d599bc877137ffb674b1f5e86
Maxim <603710@gmail.com>
2021-05-28 17:47:01 +1100

    28051746

--- JAVA SCRIPT 23032021/55/script.js
+++ JAVA SCRIPT 23032021/55/script.js
@@ -1,2 +1,33 @@
 'use strict';
 
+console.log('Запрос данных...');
+
+const register = new Promise((resolve, reject) =>{  //Promise - обещание, принимает 2 аргумента
+                                                    // resolve - обещание выполнилось
+                                                    //reject - обещание не выполнилось
+
+    setTimeout( () => {
+    console.log('Получение данных...');
+
+    const product = {
+        name: 'TV',
+        price: 500
+    };
+    resolve();
+}, 3000);
+});
+
+    register.then(() => { //метод then - отрабатывает положительный результат resolve
+    console.log('Данные получены...');
+    });
+
+
+
+
+
+
+setTimeout( () =>{
+    product.status = 'order';
+    console.log(product);
+}, 1000);
+
