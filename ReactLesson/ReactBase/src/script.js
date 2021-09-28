40a36cfc7e0ff33f09b1e5bed433331eba4ebac3
slonik1979 <novgorodzev@mail.ru>
2021-09-28 18:08:50 +1100

    Update script.js

--- ReactLesson/ReactBase/src/script.js
+++ ReactLesson/ReactBase/src/script.js
@@ -1,40 +1,53 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+const _button = button;
+export { _button as button };
+const __default = void 0;
+export { __default as default };
+
 function button() {
-    return 'button';
+  return 'button';
 }
 
 class Slider {
-    constructor(widht, height, count) {
-        this.widht = widht;
-        this.height = height;
-        this.count = count;
-    }
-    nextSlider() {
-        console.log('Moving forward')
-    }
-    prevSlider() {
-        console.log('Moving back')
-    }
-    whoAmI() {
-        console.log(this.widht, this.height, this.count)
-    }
+  constructor(widht, height, count) {
+    this.widht = widht;
+    this.height = height;
+    this.count = count;
+  }
+
+  nextSlider() {
+    console.log('Moving forward');
+  }
+
+  prevSlider() {
+    console.log('Moving back');
+  }
+
+  whoAmI() {
+    console.log(this.widht, this.height, this.count);
+  }
+
 }
 
 class AutoSlider extends Slider {
-    constructor(widht, height, count, auto) {
-        super(widht, height, count);
-        this.auto = auto;
-    }
-    play() {
-        console.log(`Autoplay: ${this.auto}`);
-    }
-}
+  constructor(widht, height, count, auto) {
+    super(widht, height, count);
+    this.auto = auto;
+  }
 
+  play() {
+    console.log("Autoplay: ".concat(this.auto));
+  }
 
-// const slider = new AutoSlider(300, 800 , 10, true);
-
+} // const slider = new AutoSlider(300, 800 , 10, true);
 // slider.whoAmI();
 // slider.play();
 
-export {button};
 
-export default Slider;
+var _default = Slider;
+const __default = _default;
+export { __default as default };
