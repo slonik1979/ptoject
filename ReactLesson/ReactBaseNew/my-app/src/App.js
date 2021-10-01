757d59509312b62516d89f27a29b1d2cd94000fd
slonik1979 <novgorodzev@mail.ru>
2021-10-01 15:27:35 +1100

    Update App.js

--- ReactLesson/ReactBaseNew/my-app/src/App.js
+++ ReactLesson/ReactBaseNew/my-app/src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
+import {Component, StrictMode} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h1>Hello World!</h1>
+}
+
+// const Field = () => {
+//   const holder = 'Введите';
+//   const styleField = {
+//     width: "300px"
+//   };
+//   return <input placeholder = {holder}
+//                 type = "text"
+//                 style = {styleField}/>
+// }
+
+function Btn() {
+  // const res = () => {
+  //   return "Login in";
+  // }
+  //const p = <p>Login in</p>
+  const logged =true;
+  const text = "Login in";
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
+class Field extends Component {
+  render() {
+    const holder = 'Введите';
+    const styleField = {
+    width: "300px"};
+
+    return <input placeholder = {holder}
+                  type = "text"
+                  style = {styleField}/>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+      <StrictMode>
+        <Header/>
+      </StrictMode>
+        
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
