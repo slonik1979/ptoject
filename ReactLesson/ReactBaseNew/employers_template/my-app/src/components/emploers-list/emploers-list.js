b302ff1b83cd9d44a007ff64d72a4ecf6ef3db1e
slonik1979 <novgorodzev@mail.ru>
2021-10-07 18:18:51 +1100

    Update emploers-list.js

--- ReactLesson/ReactBaseNew/employers_template/my-app/src/components/emploers-list/emploers-list.js
+++ ReactLesson/ReactBaseNew/employers_template/my-app/src/components/emploers-list/emploers-list.js
@@ -3,12 +3,18 @@ import EmploersListItem from "../emploers-list-item/emploers-list-item";
 
 import './emploers-list.css';
 
-const EmploersList = () => {
+const EmploersList = ({data}) => {
+    
+    const elements = data.map(item => {
+         return (
+            <EmploersListItem name={item.name} salary={item.salary}/>
+         )   
+    })
+    
     return (
+     
         <ul className="app-list list-group">
-            <EmploersListItem name="John C." salary={800}/>
-            <EmploersListItem name="Alex D." salary={3000}/>
-            <EmploersListItem name="Mike F." salary={5000}/>
+            {elements}
         </ul>
     )
 }
