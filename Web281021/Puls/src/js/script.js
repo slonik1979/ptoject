8e1daa289e2f8f8854fbbd3ec3c42dd092b8df12
slonik1979 <novgorodzev@mail.ru>
2021-11-29 17:54:59 +1100

    Update script.js

--- Web281021/Puls/src/js/script.js
+++ Web281021/Puls/src/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1300,
+        autoplaySpeed: 1000,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt="" /></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" alt="" /></button>',
         // responsive: [
