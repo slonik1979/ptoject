9837f6ed8ee9f1389f80a5bc859e4310a6d7c465
slonik1979 <novgorodzev@mail.ru>
2021-12-07 14:25:51 +1100

    Create script.js

---
+++ JAVA SCRIPT 23032021/21/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const arr = [1, 5, 10, 8, 6];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+arr.forEach(function(i, n) {
+    console.log(i, n);
+});
