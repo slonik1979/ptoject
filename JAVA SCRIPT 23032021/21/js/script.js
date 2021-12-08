7e8dcb5ba4c51904467ef36149107d002969b3fb
Maxim <novgorodzev@mail.ru>
2021-12-09 00:02:48 +1100

    09122100002

--- JAVA SCRIPT 23032021/21/js/script.js
+++ JAVA SCRIPT 23032021/21/js/script.js
@@ -1,55 +1,87 @@
 'use strict';
 
-// const arr = [1, 5, 10, 8, 6];
+// // const arr = [1, 5, 10, 8, 6];
 
-// // for (let i = 0; i < arr.length; i++) {
-// //     console.log(arr[i]);
+// // // for (let i = 0; i < arr.length; i++) {
+// // //     console.log(arr[i]);
+// // // }
+
+// // arr.forEach((i, n) => {
+// //     console.log(i, n);
+// // });
+
+// // arr.sort(num);
+// // console.log(arr);
+
+// // function num(a, b) {
+// //  return a - b;
 // // }
 
-// arr.forEach((i, n) => {
-//     console.log(i, n);
-// });
+// let num = 50;
 
-// arr.sort(num);
-// console.log(arr);
+// // while (num < 55) {
+// //     num++;
+// //     console.log(num);
+// //     //num++;
+// // }
 
-// function num(a, b) {
-//  return a - b;
+// // do {console.log(num);
+// //     num++;}
+// // while (num < 55);
+
+// for (let i =1; i <= 10; i++) {
+//     console.log(num)
+//     num++;
 // }
 
-let num = 50;
 
-// while (num < 55) {
+// let num = 10;
+
+// // while (num < 15) {
+// //     num++;
+// //     console.log(num);
+    
+// // }
+
+// // do {
+// //     num++;
+// //     console.log(num);
+// // }
+// // while (num < 15);
+
+// for (let i= 1; i < 10; i++) {
 //     num++;
 //     console.log(num);
-//     //num++;
+// }
+
+
+let num = 20;
+// while (num < 30) {
+//     console.log(num);
+//     num++;
 // }
 
 // do {console.log(num);
 //     num++;}
-// while (num < 55);
+//     while (num < 30);
 
-for (let i =1; i <= 10; i++) {
-    console.log(num)
+for (let i = 1; i < 10; i++)
+{
+    console.log(num);
     num++;
 }
 
 
-let num = 10;
+arr = [25, 2, 55, 21, 85]
 
-// while (num < 15) {
-//     num++;
-//     console.log(num);
-    
-// }
+arr.forEach(function (item) {
+    console.log(item);
+})
 
-// do {
-//     num++;
-//     console.log(num);
-// }
-// while (num < 15);
+arr.sort(sortingNumbers);
+console.log(arr);
 
-for (let i= 1; i < 10; i++) {
-    num++;
-    console.log(num);
+
+function sortingNumbers(a, b) {
+    return a - b;
 }
