7b54c36cfe3c7b4ee1841c38cd82fd7ed4e0af71
Maxim <novgorodzev@mail.ru>
2021-12-09 20:57:49 +1100

    Update script.js

--- JAVA SCRIPT 23032021/21/js/script.js
+++ JAVA SCRIPT 23032021/21/js/script.js
@@ -1,87 +1,125 @@
 'use strict';
 
-// // const arr = [1, 5, 10, 8, 6];
+// // // const arr = [1, 5, 10, 8, 6];
 
-// // // for (let i = 0; i < arr.length; i++) {
-// // //     console.log(arr[i]);
+// // // // for (let i = 0; i < arr.length; i++) {
+// // // //     console.log(arr[i]);
+// // // // }
+
+// // // arr.forEach((i, n) => {
+// // //     console.log(i, n);
+// // // });
+
+// // // arr.sort(num);
+// // // console.log(arr);
+
+// // // function num(a, b) {
+// // //  return a - b;
 // // // }
 
-// // arr.forEach((i, n) => {
-// //     console.log(i, n);
-// // });
+// // let num = 50;
 
-// // arr.sort(num);
-// // console.log(arr);
+// // // while (num < 55) {
+// // //     num++;
+// // //     console.log(num);
+// // //     //num++;
+// // // }
 
-// // function num(a, b) {
-// //  return a - b;
+// // // do {console.log(num);
+// // //     num++;}
+// // // while (num < 55);
+
+// // for (let i =1; i <= 10; i++) {
+// //     console.log(num)
+// //     num++;
 // // }
 
-// let num = 50;
 
-// // while (num < 55) {
+// // let num = 10;
+
+// // // while (num < 15) {
+// // //     num++;
+// // //     console.log(num);
+    
+// // // }
+
+// // // do {
+// // //     num++;
+// // //     console.log(num);
+// // // }
+// // // while (num < 15);
+
+// // for (let i= 1; i < 10; i++) {
 // //     num++;
 // //     console.log(num);
-// //     //num++;
+// // }
+
+
+// let num = 20;
+// // while (num < 30) {
+// //     console.log(num);
+// //     num++;
 // // }
 
 // // do {console.log(num);
 // //     num++;}
-// // while (num < 55);
+// //     while (num < 30);
 
-// for (let i =1; i <= 10; i++) {
-//     console.log(num)
-//     num++;
-// }
-
-
-// let num = 10;
-
-// // while (num < 15) {
-// //     num++;
-// //     console.log(num);
-    
-// // }
-
-// // do {
-// //     num++;
-// //     console.log(num);
-// // }
-// // while (num < 15);
-
-// for (let i= 1; i < 10; i++) {
-//     num++;
-//     console.log(num);
-// }
-
-
-let num = 20;
-// while (num < 30) {
+// for (let i = 1; i < 10; i++)
+// {
 //     console.log(num);
 //     num++;
 // }
 
-// do {console.log(num);
-//     num++;}
-//     while (num < 30);
+
+// arr = [25, 2, 55, 21, 85]
+
+// arr.forEach(function (item) {
+//     console.log(item);
+// })
+
+// arr.sort(sortingNumbers);
+// console.log(arr);
+
+
+// function sortingNumbers(a, b) {
+//     return a - b;
+// }
+
+
+
+
+
+
+
+
+
+//циклы
+let number = 10; 
+
+// while (number < 15) {
+//     console.log(number);
+//     number++;
+// }
+
+// do {console.log(number);
+//     number++;} 
+//     while (number < 15
+//     );
 
 for (let i = 1; i < 10; i++)
 {
-    console.log(num);
-    num++;
+    console.log(number);
+    number++;
 }
 
 
-arr = [25, 2, 55, 21, 85]
+//массивы перебор
 
-arr.forEach(function (item) {
-    console.log(item);
-})
-
-arr.sort(sortingNumbers);
+const arr = [4, 58, 14, 52, 25]
+arr.sort(sortNumber);
 console.log(arr);
 
-
-function sortingNumbers(a, b) {
+function sortNumber(a, b) {
     return a - b;
 }
