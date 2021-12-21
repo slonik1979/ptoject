b617a69f8d7c21a50ce3e8a27a9b7d34a94c282b
slonik1979 <novgorodzev@mail.ru>
2021-12-22 10:50:29 +1100

    2212211050

--- JAVA SCRIPT 08122021/Food/src/js/main.js
+++ JAVA SCRIPT 08122021/Food/src/js/main.js
@@ -2,29 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
-
-
-    function hideTabs() {
-        tabsContent.forEach(item => {
-            item.style.display = 'none';
-        });
-
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
-
-    function showTabs(i = 0) {
-        tabsContent[i].style.display = 'block';
-        tabs[i].classList.add('tabheader__item_active');
-    }
-
-    hideTabs();
-    showTabs();
-
+   
 
 
 });
