aa33934c5106d5f563f729e1460e093de8f5b2d3
slonik1979 <novgorodzev@mail.ru>
2021-12-22 12:17:42 +1100

    Update main.js

--- JAVA SCRIPT 08122021/Food/src/js/main.js
+++ JAVA SCRIPT 08122021/Food/src/js/main.js
@@ -2,7 +2,41 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-   
+   const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
+    function hideTabs() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabs(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+        
+    hideTabs();    
+    showTabs();
+
+
+
+    tabsParent.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, n) => {
+                if (e.target == item) {
+                    hideTabs();    
+                    showTabs(n);
+                }
+            });
+            
+
+            
+        }
+    });
 
 });
