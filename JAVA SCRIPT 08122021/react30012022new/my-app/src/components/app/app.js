b3dd8198b4c9ed8433cfca581b8b1b11528cb28e
slonik1979 <novgorodzev@mail.ru>
2022-02-01 17:25:11 +1100

    0102211725

---
+++ JAVA SCRIPT 08122021/react30012022new/my-app/src/components/app/app.js
@@ -0,0 +1,13 @@
+
+import './app.css';
+import AppInfo from '../app-info/app-info';
+
+function App() {
+    return (
+       <div className = "app" >
+           <AppInfo/>
+       </div>
+    );
+}
+
+export default App;
