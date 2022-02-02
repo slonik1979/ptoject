12dfb3d4a23c0bb02e0a5f3d3b76b2cc703e985c
slonik1979 <novgorodzev@mail.ru>
2022-02-02 17:32:59 +1100

    02022022

--- JAVA SCRIPT 08122021/react 24012022/my-app/src/App.js
+++ JAVA SCRIPT 08122021/react 24012022/my-app/src/App.js
@@ -2,27 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 
-const Header = () => {
-  return <h2>Hello</h2>
-}
 
-const Field = () => {
-  return <input placeholder='Введите имя' type="text" />
-}
-
-function Btn() {
-  const text = 'log in';
-  const logged = true;
-
-  return <button>{logged ? 'Enter' : text}</button>
-}
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Field/>
-      <Btn/>
+      
     </div>
   );
 }
