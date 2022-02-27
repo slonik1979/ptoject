302d1a2797396e1839878ff71e25f290afced241
Maxim <novgorodzev@mail.ru>
2022-02-27 22:05:03 +1100

    2702222204

--- JAVA SCRIPT 08122021/react 24012022/my-app/src/App.js
+++ JAVA SCRIPT 08122021/react 24012022/my-app/src/App.js
@@ -9,7 +9,8 @@ class WhoAmi extends Component {  //Деструктурируем
     this.state = {
       years: 37,
       experience: 10,
-      text: '+++++'
+      text: '+++++',
+      position: ''
     }
  }
     nextYear = () => {
@@ -18,17 +19,26 @@ class WhoAmi extends Component {  //Деструктурируем
           }))
       }
 
+      commitInputChanges = (e) => {
+        this.setState({
+          position: e.target.value
+        })
+      }
+
   render() {
     const {name, surname, link} = this.props;
+    const {position, years} = this.state;
     return (
       <div>
          {/* должны вызвать функцию name() */}
          <button onClick={this.nextYear} >{this.state.text}</button>
-        <h1>my names {name}, surname - {surname}, years - {this.state.years}, country - {this.state.experience} </h1> 
+        <h1>my names {name}, surname - {surname}, years - {years}, country - {this.state.experience}, 
+        position - {position}
+         </h1> 
         <a href={link}>My profile</a>
         <form>
           <span>Введите должность</span>
-          <input type="text" onChange={} />
+          <input type="text" onChange={this.commitInputChanges} />
         </form>
 
       </div>
