6698021142e97de2a6afdb5ebc82833b07ad595a
slonik1979 <novgorodzev@mail.ru>
2022-08-27 16:48:13 +1100

    2708221648

--- JAVA SCRIPT 08122021/REACT 10062022/my-app/src/components/app/app.js
+++ JAVA SCRIPT 08122021/REACT 10062022/my-app/src/components/app/app.js
@@ -1,5 +1,7 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
 
 
 import './app.css';
@@ -13,7 +15,11 @@ function App() {
 
             <div className="search-panel">
                 <SearchPanel/>
+                <AppFilter/>
             </div>
+
+            
+            <EmployeesList/>
 
         </div>
 
