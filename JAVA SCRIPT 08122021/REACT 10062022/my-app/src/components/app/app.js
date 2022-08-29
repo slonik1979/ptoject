f89f83929d4897d8fe42f1e8ea3708664318c1fb
Maxim <novgorodzev@mail.ru>
2022-08-29 21:42:01 +1100

    2908222141

--- JAVA SCRIPT 08122021/REACT 10062022/my-app/src/components/app/app.js
+++ JAVA SCRIPT 08122021/REACT 10062022/my-app/src/components/app/app.js
@@ -2,6 +2,8 @@ import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
 
 
 import './app.css';
@@ -20,6 +22,8 @@ function App() {
 
             
             <EmployeesList/>
+
+            <EmployeesAddForm/>
 
         </div>
 
