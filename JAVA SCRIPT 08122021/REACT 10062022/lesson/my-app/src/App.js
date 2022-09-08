382270e75219618c918e589ff762dfb607c75d60
slonik1979 <novgorodzev@mail.ru>
2022-09-08 17:57:11 +1100

    0809221757

--- JAVA SCRIPT 08122021/REACT 10062022/lesson/my-app/src/App.js
+++ JAVA SCRIPT 08122021/REACT 10062022/lesson/my-app/src/App.js
@@ -5,7 +5,8 @@ class WhoAmi extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       years: 25
+       years: 25,
+       position: ''
       }
     }
   
@@ -15,13 +16,27 @@ class WhoAmi extends Component {
       }))
     }
 
+    commitInnputChange = (e) => {
+      this.setState(state => ({
+        position: e.target.value
+      }))
+    }
+
   render() {
     const {name, surname, link} = this.props;
+    const {years, position} = this.state;
     return (
       <div>
         <button onClick={this.nextYear}>+++</button>
-        <h1>My names is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <h1>My names is {name}, 
+        surname - {surname}, 
+        age - {years}, 
+        position - {position} </h1>
         <a href ={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={this.commitInnputChange} />
+        </form>
       </div>
     )
   }
