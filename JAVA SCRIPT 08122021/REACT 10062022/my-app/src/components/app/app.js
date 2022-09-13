2884196b56053e6667980fd74a521fe62f66ff09
slonik1979 <novgorodzev@mail.ru>
2022-09-13 17:04:03 +1100

    1309221703

--- JAVA SCRIPT 08122021/REACT 10062022/my-app/src/components/app/app.js
+++ JAVA SCRIPT 08122021/REACT 10062022/my-app/src/components/app/app.js
@@ -17,29 +17,62 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: "Mike R.", salary: 1000, increase: false, id: 1},
-                {name: "Alex Z.", salary: 1200, increase: true, id: 2},
-                {name: "Rik F.", salary: 15000, increase: false, id: 3}
+                {name: "Mike R.", salary: 1000, increase: false, like: false, id: 1},
+                {name: "Alex Z.", salary: 1200, increase: true, like: true, id: 2},
+                {name: "Rik F.", salary: 15000, increase: false, like: false, id: 3}
             ]
         }
+        this.maxId = 4;
+    }
+    
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            like: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onToggleIncrease = (id) => {
+        console.log(`Increase ${id}`);
+    }
+
+    onToggleRise = (id) => {
+        console.log(`Rise${id}`);
     }
     
     render() {
         return (
             <div className="app">
                 <AppInfo/>
-    
                 <div className="search-panel">
                     <SearchPanel/>
                     <AppFilter/>
                 </div>
-    
-                
                 <EmployeesList
                     bases = {this.state.data}
-                    onDelete={id => console.log(id)}/>
+                    onDelete={this.deleteItem}
+                    onToggleIncrease={this.onToggleIncrease}
+                    onToggleRise={this.onToggleRise}/>
     
-                <EmployeesAddForm/>
+                <EmployeesAddForm
+                onAdd={this.addItem}/>
     
             </div>
     
--- JAVA SCRIPT 08122021/REACT 10062022/my-app/src/components/employees-list/employees-list.js
+++ JAVA SCRIPT 08122021/REACT 10062022/my-app/src/components/employees-list/employees-list.js
@@ -3,7 +3,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 
-const EmployeesList = ({bases, onDelete}) => {
+const EmployeesList = ({bases, onDelete, onToggleIncrease, onToggleRise}) => {
 
     const elements = bases.map(item => {
         const {id, ...itemP} = item; 
@@ -11,7 +11,9 @@ const EmployeesList = ({bases, onDelete}) => {
             <EmployeesListItem  key = {id} {...itemP}
             // спред оператор разворачивает item {item.name} salary = {item.salary} 
             // key = для праильного работы алгоритма согласования = для оптимизации скорости приложения
-            onDelete = {() => onDelete(id)}/> 
+            onDelete = {() => onDelete(id)}
+            onToggleIncrease = {() => onToggleIncrease(id)}
+            onToggleRise = {() => onToggleRise(id)}/> 
         )
     })
 
