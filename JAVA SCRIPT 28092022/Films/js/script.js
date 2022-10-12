5a87e3076bacb639537995619f3f990e179275f1
slonik1979 <novgorodzev@mail.ru>
2022-10-12 17:37:11 +1100

    1210221737

--- JAVA SCRIPT 28092022/Films/js/script.js
+++ JAVA SCRIPT 28092022/Films/js/script.js
@@ -42,3 +42,21 @@ personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
 
 
+for (let i = 5; i < 11; i++) {
+    console.log(i)
+}
+
+for (let i = 20; i > 10; i--) {
+    if (i === 13) {
+        break;
+    }
+    console.log(i)
+}
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+  }
+
+
