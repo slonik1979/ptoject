2cb52273cac0233847f0af1dea625d6bed7cb484
slonik1979 <novgorodzev@mail.ru>
2023-01-11 17:13:14 +1100

    1101231713

--- JAVA SCRIPT 08122021/REACT 10062022/lesson/my-app/src/App.js
+++ JAVA SCRIPT 08122021/REACT 10062022/lesson/my-app/src/App.js
@@ -1,5 +1,22 @@
 import { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
+
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+  
+`;
+
+const Header = styled.h2`
+  font-size: 20px;
+`;
+
+const Button = styled.button`
+  background-color: gold;
+`;
 
 class WhoAmi extends Component {
   constructor(props) {
@@ -26,29 +43,34 @@ class WhoAmi extends Component {
     const {name, surname, link} = this.props;
     const {years, position} = this.state;
     return (
-      <div>
-        <button onClick={this.nextYear}>+++</button>
-        <h1>My names is {name}, 
+      <EmpItem>
+        <Button onClick={this.nextYear}>+++</Button>
+        <Header>My names is {name}, 
         surname - {surname}, 
         age - {years}, 
-        position - {position} </h1>
+        position - {position} </Header>
         <a href ={link}>My profile</a>
         <form>
           <span>Введите должность</span>
           <input type="text" onChange={this.commitInnputChange} />
         </form>
-      </div>
+      </EmpItem>
     )
   }
   
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <WhoAmi name ="Alex" surname = "Klinton" link = "ok.ru"/>
       <WhoAmi name ="Mike" surname = "Bush" link = "vk.ru"/>
-    </div>
+    </Wrapper>
   );
 }
 
--- JAVA SCRIPT 08122021/react15122022/my-app/src/components/app/app.js
+++ JAVA SCRIPT 08122021/react15122022/my-app/src/components/app/app.js
@@ -5,6 +5,7 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
+
 import './app.css';
 
 class App extends Component {
