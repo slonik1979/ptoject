a1778ab048bbf52c45a796c052ec7d1570ccb6f7
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-01-19 21:34:19 +1100

    1901232134

--- JAVA SCRIPT 18012023/JS основы/app.js
+++ JAVA SCRIPT 18012023/JS основы/app.js
@@ -1,42 +1,77 @@
-console.table({ name: 'Max', age: 30 });
+// console.table({ name: 'Max', age: 30 });
 
-let number;
+// let number;
 
-// number = Math.PI;
-// number = number.toFixed(2);
+// // number = Math.PI;
+// // number = number.toFixed(2);
 
-// number = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+// // number = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
 
-// number = Math.floor(Math.random() * 100);
+// // number = Math.floor(Math.random() * 100);
 
-number =parseInt('100$');
+// number =parseInt('100$');
 
-console.log(number);
-console.log(typeof number);
+// console.log(number);
+// console.log(typeof number);
 
-let string = "some test string";
+// let string = "some test string";
 
-// let value = string.slice(0, 1).toUpperCase() + ' ' + string[string.length - 1].toUpperCase();
-    // value = string[string.length - 1];
+// // let value = string.slice(0, 1).toUpperCase() + ' ' + string[string.length - 1].toUpperCase();
+//     // value = string[string.length - 1];
 
-    value = string.indexOf(' ', string.indexOf(' ')+1);
-    value = string.slice(0, -6);
-console.log(value);
+//     value = string.indexOf(' ', string.indexOf(' ')+1);
+//     value = string.slice(0, -6);
+// console.log(value);
 
-const a = 20;
-const b = 16;
-value = `${a}` + `${b}`;
+// const a = 20;
+// const b = 16;
+// value = `${a}` + `${b}`;
 
-console.log(value);
+// console.log(value);
 
+// const phone = {
+//     product: 'iphone',
+//     price: 1000,
+//     currency: 'dollar',
+//     details: {
+//         model: '',
+//         color: ''
+//     }
+// };
+// console.log(phone);
 
-const phone = {
-    product: 'iphone',
-    price: 1000,
-    currency: 'dollar',
-    details: {
-        model: '',
-        color: ''
-    }
+let a = true + undefined;
+console.log(a);
+
+let hidden = 'hidden';
+
+if (hidden == 'hidden1') {
+  hidden = 'visible';
+  console.log(hidden);
+} else hidden == 'hidden';
+console.log(hidden);
+
+let number = -1;
+if (number == 0) {
+  number = 1;
+  console.log(number);
+} else if (number < 0) {
+  number = 'less then zero';
+  console.log(number);
+} else if (number > 0) {
+  number *= 10;
+  console.log(number);
+}
+
+let car = {
+  name: 'Lexus',
+  age: 1,
+  create: 2008,
+  needRepair: false,
 };
-console.log(phone);
+
+if (car.age > 5) {
+  car.needRepair = true;
+  console.log('Need Repair');
+  console.log(car.needRepair);
+}
