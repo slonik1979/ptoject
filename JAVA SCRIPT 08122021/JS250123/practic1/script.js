9c934c8182087968bf67d5f828f342b083cf8a08
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-01-25 23:10:50 +1100

    2501232310

--- JAVA SCRIPT 08122021/JS250123/practic1/script.js
+++ JAVA SCRIPT 08122021/JS250123/practic1/script.js
@@ -17,24 +17,53 @@
     }
 Проверить, чтобы все работало без ошибок в консоли */
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/* Задание на урок:
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
+str.length - и получить её длину)
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+4) Потренироваться и переписать цикл еще двумя способами*/
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false,
-// }
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const questi1 = prompt('Один из последних просмотренных фильмов?', '');
-// const questi2 = +prompt('На сколько оцените его?', '');
-// const questi3 = prompt('Один из последних просмотренных фильмов?', '');
-// const questi4 = +prompt('На сколько оцените его?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
-// personalMovieDB.movies[questi1] = questi2;
-// personalMovieDB.movies[questi3] = questi4;
-// personalMovieDB.movies['name'] = 'Anna';
+for (let i = 0; i < 2; i++) {
+  const questi1 = prompt('Один из последних просмотренных фильмов?', '');
+  const questi2 = +prompt('На сколько оцените его?', '');
 
-// console.log(personalMovieDB.movies);
-// console.log(personalMovieDB.movies.name);
+  if (
+    questi1 != null &&
+    questi2 != null &&
+    questi1 != '' &&
+    questi2 != '' &&
+    questi1.length < 50
+  ) {
+    personalMovieDB.movies[questi1] = questi2;
+  } else {
+    console.log('erorr');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB.movies);
