d03a79d6761f658c9a48f979b10224a6f1c2bbf3
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-01-30 23:38:01 +1100

    3001232337

---
+++ JAVA SCRIPT 08122021/JS250123/оснвы JS/script.js
@@ -0,0 +1,16 @@
+let fruits = ['Яблоко', 'Апельсин', 'Слива'];
+
+console.log(fruits[0]); // Яблоко
+
+let fruitsLenght = fruits.length;
+
+console.log(fruitsLenght);
+
+console.log(fruits);
+
+let fruitsNext = fruits[fruits.length - 1];
+
+console.log(fruitsNext);
+
+let fruitsArr = fruits.push('Banan');
+console.log(fruitsArr);
