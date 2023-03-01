70f663e65420690b734498a6211f0589b47562e2
slonik1979 <novgorodzev@mail.ru>
2023-03-01 18:18:53 +1100

    0103231818

--- JAVA SCRIPT 08122021/React17022023/самурай/my-app/src/components/dialog/dialog.js
+++ JAVA SCRIPT 08122021/React17022023/самурай/my-app/src/components/dialog/dialog.js
@@ -1,8 +1,27 @@
-import s from './dialog.module.css'
+import s from './dialog.module.css';
 
 const Dialogs = () => {
     return (
-        <div className={s.content}>Dialogs</div>
+        
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Ivan
+                </div>
+                <div className='dialog'>
+                    Peter
+                </div>
+                <div className='dialog'>
+                    Roman
+                </div>
+            </div>
+        
+       <div className={s.messeges}>
+            <div className="message">Привет</div>
+            <div className="message">Пока</div>
+            <div className="message">Как дела</div>
+            </div>
+    </div>
     )
 }
 
