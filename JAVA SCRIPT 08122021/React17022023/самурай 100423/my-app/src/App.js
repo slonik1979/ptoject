f8448c95e82ad5677e78543a92a70e9cfbcbf2d7
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-04-18 18:55:28 +1100

    1804231855

--- JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/App.js
+++ JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/App.js
@@ -1,30 +1,17 @@
-import logo from './logo.svg';
+//
 import './App.css';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <header className='header'>
+    <div className="app-wrapper">
+      <header>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_NxfO2jY2p_wn9uhwd4m2DrGPEIQU9qDzbVpMJSHPFbgJn260WLBMEibDXUpEvassgIY&usqp=CAU" />
       </header>
-      <nav className='nav'>
-        <div>
-          <a>Profile</a> 
-        </div>
-        <div>
-          <a>Messages</a> 
-        </div>
-        <div>
-          <a>News</a> 
-        </div>
-        <div>
-          <a>Music</a> 
-        </div>
-        <div>
-          <a>Settings</a> 
-        </div>
+      <nav>
+        <div>Profile</div>
+        <div>Messages</div>
       </nav>
-      <div className='content'>My content</div>
+      <div>My content</div>
     </div>
   );
 }
