259b3f201ea8b077e16b000c443614d0dafa013b
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-04-18 21:46:14 +1100

    1804232146

--- JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/App.js
+++ JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/App.js
@@ -4,14 +4,41 @@ import './App.css';
 function App() {
   return (
     <div className="app-wrapper">
-      <header>
+      <header className="header">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_NxfO2jY2p_wn9uhwd4m2DrGPEIQU9qDzbVpMJSHPFbgJn260WLBMEibDXUpEvassgIY&usqp=CAU" />
       </header>
-      <nav>
-        <div>Profile</div>
-        <div>Messages</div>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
       </nav>
-      <div>My content</div>
+
+      <div className="content">
+        <div>
+          <img src="https://tinypng.com/images/social/website.jpg" />
+        </div>
+        <div>ava + descr</div>
+        <div>
+          My post
+          <div>New post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
