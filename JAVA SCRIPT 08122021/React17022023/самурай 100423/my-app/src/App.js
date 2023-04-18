616fcb714e38a617713c82d779dcb76cff0b08a0
slonik1979 <novgorodzev@mail.ru>
2023-04-18 17:50:30 +1100

    1804231750

--- JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/App.js
+++ JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <header>
+    <div className='app-wrapper'>
+      <header className='header'>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_NxfO2jY2p_wn9uhwd4m2DrGPEIQU9qDzbVpMJSHPFbgJn260WLBMEibDXUpEvassgIY&usqp=CAU" />
       </header>
-      <nav>
-        <div>Profile</div>
-        <div>Messages</div>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a> 
+        </div>
+        <div>
+          <a>Messages</a> 
+        </div>
+        <div>
+          <a>News</a> 
+        </div>
+        <div>
+          <a>Music</a> 
+        </div>
+        <div>
+          <a>Settings</a> 
+        </div>
       </nav>
-      <div>My content</div>
+      <div className='content'>My content</div>
     </div>
   );
 }
