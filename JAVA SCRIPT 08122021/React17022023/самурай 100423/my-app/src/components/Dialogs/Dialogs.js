55c28c4ea4bc083af5f18c30e01025241ba30375
slonik1979 <novgorodzev@mail.ru>
2023-04-21 17:26:15 +1100

    2104231726

--- JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/components/Dialogs/Dialogs.js
+++ JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/components/Dialogs/Dialogs.js
@@ -2,9 +2,32 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div
-        >Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Ivan
+                </div>
+                <div className={s.dialog}>
+                    Peter
+                </div>
+                <div className={s.dialog}>
+                    Mike
+                </div>
+                <div className={s.dialog}>
+                    Mike
+                </div>
+                <div className={s.dialog}>
+                    Mike
+                </div>
+            </div>
+            <div className={s.messeges}>
+                <div className={s.messege}>Hi</div>
+                <div className={s.messege}>Hi2</div>
+                <div className={s.messege}>Hi3</div>
+            </div>
         </div>
+        
+       
     )
 }
 
--- JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/components/Navbar/Navbar.js
+++ JAVA SCRIPT 08122021/React17022023/самурай 100423/my-app/src/components/Navbar/Navbar.js
@@ -1,26 +1,29 @@
 import s from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
 
+const statusActive = ({isActive}) => ({color: isActive ? 'chartreuse' : ''});
+
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <NavLink to="/profile" />
-        Profile
+        <NavLink to="/profile" style={statusActive}>Profile</NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/dialogs" className={s.active}>
-          Messages
-        </NavLink>
+      <NavLink to="/dialogs" style={statusActive}>Messages</NavLink>
+        
       </div>
       <div className={s.item}>
-        <NavLink to="/News">News</NavLink>
+      <NavLink to="/News" style={statusActive}>News</NavLink>
+    
       </div>
       <div className={s.item}>
-        <NavLink to="/Music">Music</NavLink>
+      <NavLink to="/Music" style={statusActive}>Music</NavLink>
+      
       </div>
       <div className={s.item}>
-        <NavLink to="/Settings">Settings</NavLink>
+      <NavLink to="/Settings" style={statusActive}>Settings</NavLink>
+        
       </div>
     </nav>
   );
