93459f4a87e8b1cd1bc9bfd9675c2afdf6d3e3f2
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-08-14 22:36:55 +1100

    1408232236

--- JAVA SCRIPT 08122021/React17022023/самурай 140823/my-app/src/App.js
+++ JAVA SCRIPT 08122021/React17022023/самурай 140823/my-app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn" />
       </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://img.freepik.com/free-photo/a-colorful-hummingbird-with-a-colorful-background_1340-38746.jpg?w=2000" />
+        </div>
+        <div>ava + desc</div>
+        <div>
+          My posts
+          <div>News posts</div>
+          <div>
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
