1666f21a2cf4a07977b31ec15e7f6115198ad2f8
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-08-16 20:58:24 +1100

    1608232058

--- JAVA SCRIPT 08122021/React17022023/самурай 140823/my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ JAVA SCRIPT 08122021/React17022023/самурай 140823/my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,22 +3,21 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
-    return <div>
+  return (
+    <div>
       My posts
       <div>
         <textarea></textarea>
         <button>Add post</button>
-        
       </div>
       <div>News posts</div>
       <div className={s.posts}>
-     <Post/>
-     <Post/>
-     <Post/>
+        <Post message="Hi" like="15" />
+        <Post message="My names Maxim" like="10" />
+        <Post />
       </div>
     </div>
- 
-  
-}
+  );
+};
 
 export default MyPosts;
--- JAVA SCRIPT 08122021/React17022023/самурай 140823/my-app/src/components/Profile/MyPosts/Post/Post.jsx
+++ JAVA SCRIPT 08122021/React17022023/самурай 140823/my-app/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
-    return <div>
-      
+const Post = ({ message, like }) => {
+  return (
+    <div>
       <div className={s.item}>
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz8yayEWOmhbnKeSVvn-0Utem8t6aw8w-TM04gyt1hElUahY5paxX4XftE2Dnqz8Nq-R8&usqp=CAU'/>
-        post1
-        <div>like</div>
-        </div>
-      
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz8yayEWOmhbnKeSVvn-0Utem8t6aw8w-TM04gyt1hElUahY5paxX4XftE2Dnqz8Nq-R8&usqp=CAU" />
+        {message}
+        <div>like={like}</div>
       </div>
-
- 
-  
-}
+    </div>
+  );
+};
 
 export default Post;
