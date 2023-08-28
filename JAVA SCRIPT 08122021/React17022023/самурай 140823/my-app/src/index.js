f99dfa2da2e7c5ea5f8ff9d90380965cacd2ab56
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-08-28 22:47:02 +1100

    2808232246

--- JAVA SCRIPT 08122021/React17022023/самурай 140823/my-app/src/index.js
+++ JAVA SCRIPT 08122021/React17022023/самурай 140823/my-app/src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let postsData = [
   { id: 1, message: 'Hi', like: 15 },
-  { id: 1, message: 'My names Maxim', like: 10 },
+  { id: 2, message: 'My names Maxim', like: 10 },
+  { id: 3, message: 'Props', like: 52 },
 ];
 root.render(
   <React.StrictMode>
