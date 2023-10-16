697535696eae0ea8038cbe767cdf5acfad382a07
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-10-16 11:48:59 +1100

    1610231148

--- Android/15102023/151023/App.js
+++ Android/15102023/151023/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Данил должен изучать программирование</Text>
+      <Text>Данил должен изучать программирование!</Text>
 
       <StatusBar style="auto" />
     </View>
