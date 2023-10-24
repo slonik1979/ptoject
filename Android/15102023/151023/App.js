530b09cf5a92d89fff618674c01bc71e88f5022c
slonik1979 <novgorodzev@mail.ru>
2023-10-24 17:51:37 +1100

    2410231751

--- Android/15102023/151023/App.js
+++ Android/15102023/151023/App.js
@@ -5,27 +5,28 @@ import { Text, View, Alert } from 'react-native';
 import { Post } from './components/Post';
 
 export default function App() {
-  const [items, setItems] = React.useState([]);
+  // const [items, setItems] = React.useState([]);
 
-  React.useEffect(() => {
-    axios
-      .get('https://652fb0e66c756603295d7f2c.mockapi.io/18102023')
-      .then(({ data }) => {
-        setItems(data);
-      })
-      .catch((err) => {
-        console.log(err);
-        Alert.alert('Ошибка', 'Не удалось');
-      });
-  }, []);
+  // React.useEffect(() => {
+  //   axios
+  //     .get('https://652fb0e66c756603295d7f2c.mockapi.io/18102023')
+  //     .then(({ data }) => {
+  //       setItems(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       Alert.alert('Ошибка', 'Не удалось');
+  //     });
+  // }, []);
 
-  const Post1 = items.map((obj) => (
-    <Post title={obj.title} createData={obj.createData} image={obj.image} />
-  ));
+  // const Post1 = items.map((obj) => (
+  //   <Post title={obj.title} createData={obj.createData} image={obj.image} />
+  // ));
 
   return (
     <View>
-      <Post1 />
+
+      <Post  title ='Тест' image ='https://img.freepik.com/free-photo/a-cupcake-with-a-strawberry-on-top-and-a-strawberry-on-the-top_1340-35087.jpg?w=740&t=st=1698126696~exp=1698127296~hmac=98392d6c036b56cf934f30a95ede67cc16075fac53b6c843ee9481f19f15c151' createData='24/10/2023'/>
     </View>
   );
 }
