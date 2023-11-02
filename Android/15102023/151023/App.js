0158332653a88c40a3f6add52f10be733971c7ca
slonik1979 <novgorodzev@mail.ru>
2023-11-02 17:42:47 +1100

    0211231742

--- Android/15102023/151023/App.js
+++ Android/15102023/151023/App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import axios from 'axios';
-import { Text, View, Alert } from 'react-native';
+import { Text, View, Alert, FlatList, ActivityIndicator } from 'react-native';
 import { Post } from './components/Post';
 
 export default function App() {
+  const [isLoding, setIsLoding] = React.useState(true);
   const [items, setItems] = React.useState([]);
 
-  React.useEffect(() => {
+  const fetchPosts = () => {
+    setIsLoding(true);
     axios
       .get('https://652fb0e66c756603295d7f2c.mockapi.io/18102023')
       .then(({ data }) => {
@@ -16,19 +18,45 @@ export default function App() {
       .catch((err) => {
         console.log(err);
         Alert.alert('Ошибка', 'Не удалось');
-      });
-  }, []);
+      }).finally(() => {
+        setIsLoding(false);
+      })
+  };
+
+  React.useEffect(fetchPosts, []);
 
   // const Post1 = items.map((obj) => (
   //   <Post title={obj.title} createData={obj.createData} image={obj.image} />
   // ));
 
+  if(isLoding) {
+    return <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <ActivityIndicator size='large'/>
+      <Text style={{marginTop: 15}}>Загрузка...</Text>
+    </View>
+  }
+
+  // style={{
+  //   flex: 1,
+  //   justify-content: 'center',
+  //   alignItems: 'center'
+  // }}
+
   return (
     <View>
- {items.map((obj) => (
+      
+      <FlatList
+      data={items} renderItem={({item}) => <Post title={item.title} image={item.image}
+      createData={item.createData}/>}
+      />
+ {/* {items.map((obj) => (
     <Post title={obj.title} createData={obj.createData} image={obj.image} />
-   ))}
-      <Post  title ='Тест' image ='https://img.freepik.com/free-photo/a-cupcake-with-a-strawberry-on-top-and-a-strawberry-on-the-top_1340-35087.jpg?w=740&t=st=1698126696~exp=1698127296~hmac=98392d6c036b56cf934f30a95ede67cc16075fac53b6c843ee9481f19f15c151a' createData='24/10/2023'/>
+   ))} */}
+      {/* <Post  title ='Тест' image ='https://img.freepik.com/free-photo/a-cupcake-with-a-strawberry-on-top-and-a-strawberry-on-the-top_1340-35087.jpg?w=740&t=st=1698126696~exp=1698127296~hmac=98392d6c036b56cf934f30a95ede67cc16075fac53b6c843ee9481f19f15c151a' createData='24/10/2023'/> */}
     </View>
   );
 }
