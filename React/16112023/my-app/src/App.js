96e094ef90c927eba31e2e89902cc8e143a349e6
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-11-16 22:31:41 +1100

    1611232231

--- React/16112023/my-app/src/App.js
+++ React/16112023/my-app/src/App.js
@@ -1,16 +1,14 @@
-
 import './App.css';
+import Content from './components/Content/Content';
+import Header from './components/Header/Header';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <header className='header'> 
-        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNQE0RcpStvxnE8SjJ-DHzqUzF3DrIc203E0UloXnG_qVVD44hwB0LPCr0L6aQQ_WzXF8&usqp=CAU'></img>
-      </header>
-      <nav className='nav'>
-        <div>Profile</div>
-      </nav>
-      <div className='content'>Main content</div>
+    <div className="app-wrapper">
+      <Header />
+      <NavBar />
+      <Content />
     </div>
   );
 }
---
+++ React/16112023/my-app/src/components/Content/Content.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Content.css';
+
+const Content = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://cdn.auckland.ac.nz/aem/content/auckland/en/news/2022/06/08/how-nz-can-live-up-to-clean-green-image/jcr:content/leftpar/imagecomponent/image.img.1024.medium.jpg/1654727864784.jpg" />
+      </div>
+      <div>Avatar + desc</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
