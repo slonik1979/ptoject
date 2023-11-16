895b32a27353ae5bd1955c8abb573d78a10a2109
slonik1979 <novgorodzev@mail.ru>
2023-11-16 17:42:23 +1100

    1611231742

---
+++ React/16112023/my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='app-wrapper'>
+      <header className='header'> 
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNQE0RcpStvxnE8SjJ-DHzqUzF3DrIc203E0UloXnG_qVVD44hwB0LPCr0L6aQQ_WzXF8&usqp=CAU'></img>
+      </header>
+      <nav className='nav'>
+        <div>Profile</div>
+      </nav>
+      <div className='content'>Main content</div>
+    </div>
+  );
+}
+
+export default App;
