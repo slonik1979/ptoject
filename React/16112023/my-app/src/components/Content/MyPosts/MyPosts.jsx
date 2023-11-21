4a1bc86b3ec8dc5b351c694ec255efe829a1533f
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2023-11-21 21:19:13 +1100

    211120232119

--- React/16112023/my-app/src/components/Content/MyPosts/MyPosts.jsx
+++ React/16112023/my-app/src/components/Content/MyPosts/MyPosts.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import Post from './Post/Post';
 
 const MyPosts = () => {
-
   let myPostsData = [
-    {id: 1, message: 'Привет', like:"15"},
-    {id: 2, message: 'Как дела', like:"25"}
-  ]
+    { id: 1, message: 'Привет', like: '15' },
+    { id: 2, message: 'Как дела', like: '25' },
+  ];
+
+  let newMyPostsData = myPostsData.map((el) => (
+    <Post id={el.id} message={el.message} like={el.like} />
+  ));
 
   return (
     <div l>
       <textarea></textarea>
       <button>Add post</button>
       <div>New post</div>
-      <Post message={myPostsData[0].message} like="15" />
-      <Post message="Как дела" like="25" />
+      {newMyPostsData}
     </div>
   );
 };
--- React/16112023/my-app/src/components/Dialogs/Dialogs.jsx
+++ React/16112023/my-app/src/components/Dialogs/Dialogs.jsx
@@ -5,25 +5,31 @@ import DialogsItems from './DialogsItems/DialogsItems';
 import Message from './Message/Message';
 
 const Dialogs = () => {
-
   let dialogsData = [
-    {id: 1, name: "Max"},
-    {id: 2, name: "Petr"},
-    {id: 3, name: "Mikle"}
-  ]
+    { id: 1, name: 'Max' },
+    { id: 2, name: 'Petr' },
+    { id: 3, name: 'Mikle' },
+    { id: 4, name: 'Denis' },
+  ];
+
+  let messagesData = [
+    { id: 1, message: 'Привет' },
+    { id: 2, message: 'Как дела?' },
+    { id: 3, message: 'Круто' },
+  ];
+
+  let newDialogsData = dialogsData.map((el) => (
+    <DialogsItems id={el.id} name={el.name} />
+  ));
+
+  let newMessagesData = messagesData.map((el) => (
+    <Message id={el.id} message={el.message} />
+  ));
 
   return (
     <div className={classes.dialogs}>
-      <div className={classes.dialogsItems}>
-        <DialogsItems id="1" name="Max" />
-        <DialogsItems id="2" name="Petr" />
-        <DialogsItems id="3" name="Mikle" />
-      </div>
-      <div className={classes.messages}>
-        <Message message="Привет" />
-        <Message message="Как дела?" />
-        <Message message="Круто" />
-      </div>
+      <div className={classes.dialogsItems}>{newDialogsData}</div>
+      <div className={classes.messages}>{newMessagesData}</div>
     </div>
   );
 };
