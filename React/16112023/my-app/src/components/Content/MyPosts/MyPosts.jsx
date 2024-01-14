df7c48118ce4202e256e4cf30d8c3d9e65189e85
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-01-14 19:50:16 +1100

    1401241524

--- React/16112023/my-app/src/components/Content/MyPosts/MyPosts.jsx
+++ React/16112023/my-app/src/components/Content/MyPosts/MyPosts.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Post from './Post/Post';
 import classes from './MyPosts.module.css';
+import {
+  addPostActionCreator,
+  updateNewPostTextActionCreator,
+} from '../../../redux/state';
 
 const MyPosts = (props) => {
   let messagesElement = props.posts.map((el) => (
@@ -10,12 +14,12 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef();
 
   let addPost = () => {
-    props.dispatch({ type: 'ADD-POST' });
+    props.dispatch(addPostActionCreator());
   };
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+    let action = updateNewPostTextActionCreator(text);
     props.dispatch(action);
   };
 
--- React/16112023/my-app/src/redux/state.js
+++ React/16112023/my-app/src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = {};
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
   _state: {
     contentPage: {
@@ -37,7 +40,7 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === 'ADD-POST') {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 4,
         name: this._state.contentPage.newPostText,
@@ -46,11 +49,19 @@ let store = {
       this._state.contentPage.posts.push(newPost);
       this._state.contentPage.newPostText = '';
       this._callSubscriber(this._state);
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.contentPage.newPostText = action.newText;
       this._callSubscriber(this._state);
     }
   },
+};
+
+export const addPostActionCreator = () => {
+  return { type: ADD_POST };
+};
+
+export const updateNewPostTextActionCreator = (text) => {
+  return { type: UPDATE_NEW_POST_TEXT, newText: text };
 };
 
 export default store;
