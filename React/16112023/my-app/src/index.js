682da647c34c444c0cb8d0a8ca45a0a9e81ccabc
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-02-04 22:51:55 +1100

    0402242251

--- React/16112023/my-app/src/index.js
+++ React/16112023/my-app/src/index.js
@@ -18,4 +18,7 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+  let state = store.getState();
+  rerenderEntireTree(state);
+});
