cd73c8023c870ee7e07bf092d0c035dd4d853b15
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-02-09 20:26:12 +1100

    0902242026

--- React/05022024/my-app/src/App.js
+++ React/05022024/my-app/src/App.js
@@ -10,27 +10,36 @@ import Setting from './components/Setting/Setting';
 import Products from './components/Products/Products';
 
 function App(props) {
-
-
-
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        {/* <Profile/> */}
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route path="/profile" element={<Profile state={props.state.profilePage} />} />
-            <Route path="/dialogs/*" element={<Dialogs state={props.state.dialogsPage} messages={props.state.dialogsPage}/>} />
-            <Route path="/news" element={<News />} />
-            <Route path="/music" element={<Music />} />
-            <Route path="/setting" element={<Setting />} />
-            <Route path="/products" element={<Products state={props.state.productsPage}/>} />
-          </Routes>
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      {/* <Profile/> */}
+      <div className="app-wrapper-content">
+        <Routes>
+          <Route
+            path="/profile"
+            element={<Profile state={props.state.profilePage} />}
+          />
+          <Route
+            path="/dialogs/*"
+            element={
+              <Dialogs
+                state={props.state.dialogsPage}
+                messages={props.state.dialogsPage}
+              />
+            }
+          />
+          <Route path="/news" element={<News />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route
+            path="/products"
+            element={<Products state={props.state.productsPage} />}
+          />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
