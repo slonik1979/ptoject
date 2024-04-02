041e5e575832cb89cab2890bd41d376d055451ab
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-04-02 23:40:11 +1100

    0204241512

--- React/05022024/my-app/src/index.js
+++ React/05022024/my-app/src/index.js
@@ -1,29 +1,26 @@
-
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import state, { addMessage, addPost,  addProduct,  subscribe,  updateNewMessageText, updateNewPostText, updateNewProductName } from './redux/state';
-
+import store from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let rerenderEntireTree = (state) => {
-    
-    root.render
-    
-    (<App state={state}
-                   
-                   addPost={addPost}
-                   updateNewPostText={updateNewPostText}
-                   addMessage={addMessage}
-                   updateNewMessageText={updateNewMessageText}
-                   addProduct={addProduct} 
-                   updateNewProductName={updateNewProductName}/>);
+  root.render(
+    <App
+      state={store.getState()}
+      addPost={store.addPost}
+      updateNewPostText={store.updateNewPostText}
+      addMessage={store.addMessage}
+      updateNewMessageText={store.updateNewMessageText}
+      addProduct={store.addProduct}
+      updateNewProductName={store.updateNewProductName}
+    />
+  );
 };
 
+rerenderEntireTree(store.getState());
 
-rerenderEntireTree(state);
-
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
