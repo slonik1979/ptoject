5ca3088697c58a662347c4000a90073c3c3249c3
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-04-05 21:59:48 +1100

    0504242159

---
+++ React/fundament/05042024/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div>111</div>
+      <div>111</div>
+      <div>111</div>
+      <div>111</div>
+      <div>111</div>
+      <button>Incr</button>
+      <button>Decr</button>
+    </div>
+  );
+}
+
+export default App;
