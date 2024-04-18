a854f92fabede8cf6706ae35890a68de66484fcf
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-04-18 22:26:29 +1100

    1804242225

--- React/REDUX/my-app/src/index.js
+++ React/REDUX/my-app/src/index.js
@@ -2,7 +2,69 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { legacy_createStore } from 'redux';
 
+const intialState = { value: 0 };
+
+const reducer = (state = intialState, action) => {
+  switch (action.type) {
+    case 'INC':
+      return {
+        ...state,
+        value: state.value + 1,
+      };
+
+    case 'DEC':
+      return {
+        ...state,
+        value: state.value - 1,
+      };
+    case 'RND':
+      return {
+        ...state,
+        value: state.value * action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = legacy_createStore(reducer);
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState().value;
+};
+
+store.subscribe(update);
+
+const inc = () => ({
+  type: 'INC',
+});
+
+const dec = () => ({
+  type: 'DEC',
+});
+
+const rnd = (value) => ({
+  type: 'RND',
+  payload: value,
+});
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(value));
+});
+
+//let state = reducer(intialState, { type: 'INC' });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +76,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
