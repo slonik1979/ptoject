8b26dcf902b441aef5d76aae2aa0117e7398a771
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-04-19 17:24:10 +1100

    1904231724

---
+++ React/REDUX/my-app/src/action.js
@@ -0,0 +1,12 @@
+export const inc = () => ({
+    type: 'INC',
+  });
+  
+  export const dec = () => ({
+    type: 'DEC',
+  });
+  
+  export const rnd = (value) => ({
+    type: 'RND',
+    payload: value,
+  });
--- React/REDUX/my-app/src/index.js
+++ React/REDUX/my-app/src/index.js
@@ -2,66 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { legacy_createStore } from 'redux';
+import { bindActionCreators, legacy_createStore } from 'redux';
+import * as actions from './action';
+import reducer from './reducer';
 
-const intialState = { value: 0 };
-
-const reducer = (state = intialState, action) => {
-  switch (action.type) {
-    case 'INC':
-      return {
-        ...state,
-        value: state.value + 1,
-      };
-
-    case 'DEC':
-      return {
-        ...state,
-        value: state.value - 1,
-      };
-    case 'RND':
-      return {
-        ...state,
-        value: state.value * action.payload,
-      };
-
-    default:
-      return state;
-  }
-};
 
 const store = legacy_createStore(reducer);
 
+const {dispatch, subscribe, getState} = store;
+
 const update = () => {
-  document.getElementById('counter').textContent = store.getState().value;
+  document.getElementById('counter').textContent = getState().value;
 };
 
-store.subscribe(update);
+subscribe(update);
 
-const inc = () => ({
-  type: 'INC',
-});
 
-const dec = () => ({
-  type: 'DEC',
-});
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-const rnd = (value) => ({
-  type: 'RND',
-  payload: value,
-});
+document.getElementById('inc').addEventListener('click', inc);
 
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc());
-});
-
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec());
-});
+document.getElementById('dec').addEventListener('click', dec);
 
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(value));
+  rnd(value);
 });
 
 //let state = reducer(intialState, { type: 'INC' });
