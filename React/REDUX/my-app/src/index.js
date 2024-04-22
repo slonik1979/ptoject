9c651a5e94588cd8ea7802842436bfd52e0e368c
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-04-22 21:37:17 +1100

    2204242137

--- React/REDUX/my-app/src/index.js
+++ React/REDUX/my-app/src/index.js
@@ -3,31 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import { bindActionCreators, legacy_createStore } from 'redux';
-import * as actions from './action';
+
 import reducer from './reducer';
-import Counter from './components/Counter';
+
+import App from './components/App';
+import { Provider } from 'react-redux';
 
 const store = legacy_createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+// const { dispatch, subscribe, getState } = store;
+// const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-const update = () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <Counter
-        counter={getState().value}
-        inc={inc}
-        dec={dec}
-        rnd={() => {
-          const value = Math.floor(Math.random() * 10);
-          rnd(value);
-        }}
-      />
-    </React.StrictMode>
-  );
-};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
 
-update();
-subscribe(update);
+// update();
+// subscribe(update);
