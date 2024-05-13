8e45f867b732a09605bf6e95d8fd6227c50798fa
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-05-13 14:09:59 +1100

    1305241404

---
+++ React/05022024/55 - React JS практика - pagination, постраничный вывод пользователей/src/redux/store.js
@@ -0,0 +1,59 @@
+import dialogsReducer from './dialogs-reducer';
+import productsReducer from './products-reducer';
+import profileReducer from './profile-reducer';
+
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { message: 'Привет', id: 1, like: 10 },
+        { message: 'Пока', id: 2, like: 25 },
+      ],
+      newPostText: '8888',
+    },
+    dialogsPage: {
+      dialogs: [
+        { name: 'Иван', id: 1 },
+        { name: 'Николай', id: 2 },
+        { name: 'Света', id: 3 },
+      ],
+      messages: [
+        { message: 'Иван', id: 1 },
+        { message: 'Как дела', id: 2 },
+        { message: 'Как Вас зовут', id: 3 },
+      ],
+      newMessageBody: '999',
+    },
+    productsPage: {
+      products: [
+        { id: 1, product: 'Яблоки', price: 250, discount: 10 },
+        { id: 2, product: 'Бананы', price: 150, discount: 10 },
+        { id: 3, product: 'Мандарины', price: 100, discount: 10 },
+      ],
+      newProductName: 'fhhhff',
+    },
+  },
+
+  _callSubsCriber() {},
+
+  getState() {
+    return this._state;
+  },
+
+  subscribe(observer) {
+    this._callSubsCriber = observer;
+  },
+
+  dispatch(action) {
+    this._state.profilePage = profileReducer(this._state.profilePage, action);
+    this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+    this._state.productsPage = productsReducer(
+      this._state.productsPage,
+      action
+    );
+
+    this._callSubsCriber(this._state);
+  },
+};
+
+export default store;
