6d20a276ce3100a22c99e8fd9b8b9b7630483cc9
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-05-16 22:36:18 +1100

    1605242236

--- React/05022024/my-app/src/redux/auth-reducer.js
+++ React/05022024/my-app/src/redux/auth-reducer.js
@@ -1,7 +1,7 @@
 const SET_USER_DATA = 'SET_USER_DATA';
 
 let initialState = {
-  id: null,
+  userId: null,
   email: null,
   login: null,
   isAuth: false,
@@ -21,11 +21,9 @@ const authReducer = (state = initialState, action) => {
   }
 };
 
-export const setAuthUserData = (id, email, login) => {
-  return {
-    type: SET_USER_DATA,
-    data: { id, email, login },
-  };
-};
+export const setAuthUserData = (userId, email, login) => ({
+  type: SET_USER_DATA,
+  data: { userId, email, login },
+});
 
 export default authReducer;
