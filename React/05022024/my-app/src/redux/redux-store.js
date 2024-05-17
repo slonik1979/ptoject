4c02d0f9457d06f8c2e3e7624e6869a4e2e65c4a
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-05-17 23:38:57 +1100

    1705242338

--- React/05022024/my-app/src/redux/redux-store.js
+++ React/05022024/my-app/src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import productsReducer from './products-reducer';
 import usersReducer from './users-reducer';
+import authReducer from './auth-reducer copy';
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   productsPage: productsReducer,
   usersPage: usersReducer,
+  auth: authReducer,
 });
 
 let store = legacy_createStore(reducers);
