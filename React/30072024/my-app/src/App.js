8d28e116f282deebf8f9883719bee4310c2404ae
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-07-31 22:59:39 +1100

    3107242259

---
+++ React/30072024/my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4nKnXI_b_GU0wpcYBkUMjGy8GcBsuliQ3ZvqzQhlcT-NSCL4IJ91H7BMniX48fr-Bh5c&usqp=CAU"></img>
+      </header>
+      <nav>
+        <div>Profile</div>
+        <div>Messages</div>
+      </nav>
+      <div>My content</div>
+    </div>
+  );
+}
+
+export default App;
