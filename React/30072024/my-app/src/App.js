f83ba6e60069853a2fc929ec06c6af5f70b830b1
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-08-05 21:49:27 +1100

    0508242149

--- React/30072024/my-app/src/App.js
+++ React/30072024/my-app/src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/NavBar';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div>
-      <header>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4nKnXI_b_GU0wpcYBkUMjGy8GcBsuliQ3ZvqzQhlcT-NSCL4IJ91H7BMniX48fr-Bh5c&usqp=CAU"></img>
-      </header>
-      <nav>
-        <div>Profile</div>
-        <div>Messages</div>
-      </nav>
-      <div>My content</div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ React/30072024/my-app/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzzeMhg4ycv1R2fRdGLNloURRYJYS2AgAL6g&s"></img>
+      </div>
+      <div>ava + desc</div>
+      <div>
+        My posts
+        <div>New posts</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+          <div>post 3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
