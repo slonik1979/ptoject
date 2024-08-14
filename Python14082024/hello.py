47444e51bcade7f40b14cdceaa0d30a1f350edf1
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-08-14 17:20:40 +1100

    140820241720

---
+++ Python14082024/hello.py
@@ -0,0 +1,21 @@
+
+
+
+a=int(input())
+next = a + 1
+previous = a - 1
+
+print('The next number for the number ' + str(a) + ' is ' + str(next) +'.')
+print('The previous number for the number ' + str(a) + ' is ' + str(previous) +'.')
+
+   
+  
+    
+    
+     #print("The next number for the number", (a), "is" (a+1), ".")
+     
+     #print(a)
+     #a = int(input())
+     #print('The next number for the number ' + str(a) + ' is ' + str(a + 1) + '.')
+    # print('The previous number for the number ' + str(a) + ' is ' + str(a - 1) + '.')
+  
