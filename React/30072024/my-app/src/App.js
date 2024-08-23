e12e7f50b5125b2b74917a6f15bef798066c8edb
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-08-23 22:51:59 +1100

    2308242251

--- React/30072024/my-app/src/App.js
+++ React/30072024/my-app/src/App.js
@@ -4,18 +4,22 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
-      </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+        </div>
 
-      {/* <Profile /> */}
-    </div>
+        {/* <Profile /> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
