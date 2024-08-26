d02716997b8f27b7fa77805ee921e2d24a159e77
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-08-26 21:30:05 +1100

    26082542130

--- React/30072024/my-app/src/App.js
+++ React/30072024/my-app/src/App.js
@@ -4,49 +4,27 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Route } from 'react-router-dom';
-<<<<<<< HEAD
-=======
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import Goods from './components/Goods/Goods';
->>>>>>> 203e59401fca706bacfa9a58a16f6fe69d63f63c
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <div className="app-wrapper">
         <Header />
         <Navbar />
         <div className="app-wrapper-content">
           <Route path="/dialogs" component={Dialogs} />
           <Route path="/profile" component={Profile} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/goods" component={Goods} />
         </div>
-
-        {/* <Profile /> */}
       </div>
-=======
-   
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Route path="/dialogs" component={Dialogs} />
-        <Route path="/profile" component={Profile}/>
-        <Route path="/news" component={News}/>
-        <Route path="/music" component={Music}/>
-        <Route path="/settings" component={Settings}/>
-        <Route path="/goods" component={Goods}/>
-
-        
-        
-      </div>
-
-      {/* <Profile /> */}
-    </div>
->>>>>>> 203e59401fca706bacfa9a58a16f6fe69d63f63c
     </BrowserRouter>
   );
 }
--- React/30072024/my-app/src/components/Dialogs/Dialogs.jsx
+++ React/30072024/my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,30 @@
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-  <div className={s.dialogs}>
-    <div className={s.dialogsItems}>
-    <div className={s.dialog}>Дмитрий</div>
-    <div className={s.dialog}>Андрей</div>
-    <div className={s.dialog}>Света</div>
-    <div className={s.dialog}>Саша</div>
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/1"> Дмитрий </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/2"> Андрей </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/3"> Света </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/4"> Саша </NavLink>
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Привет</div>
+        <div className={s.message}>Пока</div>
+        <div className={s.message}>Хай</div>
+      </div>
     </div>
-    <div className={s.messages}>
-      <div className={s.message}>Привет</div>
-      <div className={s.message}>Пока</div>
-      <div className={s.message}>Хай</div>
-    </div>
-  </div>
-  
-  )
+  );
 };
 
 export default Dialogs;
