112258b2adddd85b90e43c9171d66898851ae9a3
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-09-18 22:05:56 +1100

    1809242205

--- React/30072024/my-app/src/Redux/users-reducer.js
+++ React/30072024/my-app/src/Redux/users-reducer.js
@@ -1,45 +1,55 @@
-// const ADD_POST = 'ADD-POST';
-// const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const FOLLOW = 'FOLLOW';
+const ONFOLLOW = 'ONFOLLOW';
 
 let initialState = {
   users: [
-    { id: 1, followed: false, fullname: 'Дмитрий', status: 'I am a boss', location: {city:'Minsk', country: 'Belarus'}},
-    { id: 1, followed: true, fullname: 'Андрей', status: 'I am a boss', location: {city:'Moscow', country: 'Russia'}},
-    { id: 1, followed: false, fullname: 'Света', status: 'I am a boss', location: {city:'Kiev', country: 'Ukraine'}},
+    {
+      id: 1,
+      followed: false,
+      fullname: 'Дмитрий',
+      status: 'I am a boss',
+      location: { city: 'Minsk', country: 'Belarus' },
+    },
+    {
+      id: 1,
+      followed: true,
+      fullname: 'Андрей',
+      status: 'I am a boss',
+      location: { city: 'Moscow', country: 'Russia' },
+    },
+    {
+      id: 1,
+      followed: false,
+      fullname: 'Света',
+      status: 'I am a boss',
+      location: { city: 'Kiev', country: 'Ukraine' },
+    },
   ],
   newPostText: '88888888',
 };
 
 const usersReducer = (state = initialState, action) => {
-
-
-  //switch (action.type) {
-    //     case ADD_POST: 
-  
-    //   let text = state.newPostText 
+  switch (action.type) {
+    case FOLLOW:
+    //   let text = state.newPostText
     //   return {
     //     ...state,
     //     newPostText: '',
     //     postsData: [...state.postsData, {id: 3, message: state.newPostText, likesCount: 121,}]
-    //   }
-       
-    // case UPDATE_NEW_POST_TEXT: {
+
+    case ONFOLLOW:
     //   return {
     //     ...state,
     //     newPostText: action.newText
     //   }
-     
     // }
-    // default:
-    //   return state;
-  //}
+    default:
+      return state;
+  }
 };
 
+export const followAc = (userId) => ({ type: FOLLOW, userId });
+
+export const onfollowAc = (userId) => ({ type: ONFOLLOW, userId });
+
 export default usersReducer;
-
-// export const addPostActionCreator = () => ({ type: ADD_POST });
-
-// export const updateNewPostTextActionCreate = (text) => ({
-//   type: UPDATE_NEW_POST_TEXT,
-//   newText: text,
-// });
