a0e5aa969033554180c9cbd8b2931f5647ac7784
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-11-01 22:25:29 +1100

    011120242224

--- BOOTSTRAP08102024/assets/js/main.js
+++ BOOTSTRAP08102024/assets/js/main.js
@@ -1,3 +1,9 @@
 window.addEventListener('scroll', function () {
-    document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
+  document
+    .getElementById('header-nav')
+    .classList.toggle('headernav-scroll', window.scrollY > 135);
+});
+
+$(document).ready(function () {
+  $('.owl-carousel-full').owlCarousel();
 });
