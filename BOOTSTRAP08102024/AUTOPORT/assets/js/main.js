a467082d51245498a329cf396eca769725e870d7
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-11-05 11:23:36 +1100

    052220241123

---
+++ BOOTSTRAP08102024/AUTOPORT/assets/js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', function () {
+  document
+    .getElementById('header-nav')
+    .classList.toggle('headernav-scroll', window.scrollY > 135);
+});
+
+$(document).ready(function () {
+  $('.owl-carousel-full').owlCarousel({
+    margin: 20,
+
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
