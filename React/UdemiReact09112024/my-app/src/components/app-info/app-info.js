9edd697ef7b5506e751cc36639ad511508e15122
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-11-10 16:48:45 +1100

    454555545

---
+++ React/UdemiReact09112024/my-app/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрудников в компании Автопорт</h1>
+      <h2>Общее число сотрудников:</h2>
+      <h2>Премию получают:</h2>
+    </div>
+  );
+};
+
+export default AppInfo;
