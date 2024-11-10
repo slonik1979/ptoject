1f2a2b444e49c1ee8d11ee5cd086892bd8f6ecda
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-11-10 19:24:19 +1100

    4545454554

---
+++ React/UdemiReact10112024/my-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,15 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Найти сотрудника"
+      />
+    </div>
+  );
+};
+
+export default SearchPanel;
