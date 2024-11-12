b77b696d874d057ec63ad5c4aed17ffc4c291ec9
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-11-12 23:06:11 +1100

    121120242306

--- React/UdemiReact10112024/my-app/src/components/app/app.js
+++ React/UdemiReact10112024/my-app/src/components/app/app.js
@@ -7,6 +7,11 @@ import SearchPanel from '../search-panel/search-panel';
 import './app.css';
 
 const App = () => {
+  const data = [
+    { name: 'John C.', salary: 800, increase: false },
+    { name: 'Alex M.', salary: 3000, increase: true },
+    { name: 'Carl W.', salary: 5000, increase: false },
+  ];
   return (
     <div className="app">
       <AppInfo />
@@ -14,7 +19,7 @@ const App = () => {
         <SearchPanel />
         <AppFilter />
       </div>
-      <EmployeesList />
+      <EmployeesList data={data} />
       <EmployeesAddForm />
     </div>
   );
--- React/UdemiReact10112024/my-app/src/components/employees-list-item/employees-list-item.js
+++ React/UdemiReact10112024/my-app/src/components/employees-list-item/employees-list-item.js
@@ -1,13 +1,18 @@
 import './employees-list-item.css';
 
-const EmployeesListItem = () => {
+const EmployeesListItem = (props) => {
+  let classNames = 'list-group-item d-flex justify-content-between';
+  if (props.increase) {
+    classNames += ' increase';
+  }
+
   return (
-    <li className="list-group-item d-flex justify-content-between">
-      <span className="list-group-item-label">John Smith</span>
+    <li className={classNames}>
+      <span className="list-group-item-label">{props.name}</span>
       <input
         type="text"
         className="list-group-item-input"
-        defaultValue="1000$"
+        defaultValue={props.salary + ' $'}
       />
       <div className="d-flex justify-content-center align-items-center">
         <button type="button" className="btn-cookie btn-sm ">
