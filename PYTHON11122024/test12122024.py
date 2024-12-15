27f994696561710ef0c850de1de10e409a50bacc
slonik1979 <68983652+slonik1979@users.noreply.github.com>
2024-12-15 20:17:15 +1100

    15122024

---
+++ PYTHON11122024/test12122024.py
@@ -0,0 +1,31 @@
+#number = int(input())
+#p = input()
+#s = f'Я больше никогда не буду писать "{p}"! \n'
+#print(s * number)
+
+#time = int(input())
+#child =int(input())
+#print(int(child*time/2))
+
+#print(n % 10)
+#print(n // 10 % 10)
+#print(n // 10 // 10 % 10)
+
+#name = input()
+#number = int(input())
+#a = number // 10 // 10 % 10
+#k = number // 10 % 10
+#n = number % 10
+#print("Группа №", a, ".", sep="" )
+#print(n, ".", " ", name, ".", sep="" )
+#print("Шкафчик:", " ", number, ".", sep="" )
+#print("Кроватка:"," ", k, ".", sep="" )
+
+
+number = int(input())
+a = number // 10 // 10 % 10
+b = number // 10 // 10 // 10 % 10
+c = n = number % 10
+d = number // 10 % 10
+print(a, b, c, d, sep="")
+
